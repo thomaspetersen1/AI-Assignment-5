63eaefa41da0c60bebb4146d490a281c964fa108
Thomas Petersen <petersente64@gmail.com>
2025-11-13 22:41:17 -0600

    Task 4: K-Nearest Neighbor

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ https://www.youtube.com/watch?v=3giTXZbyf1Q
 
 Task3 Kmeans Clustering:
 https://www.youtube.com/watch?v=6UF5Ysk_2gk&t=766s
+
+Task4 K-Nearest Neighbor:
+https://www.youtube.com/watch?v=b6uHw7QW_n4
+https://www.youtube.com/watch?v=V33JX4cFls4
--- hw5_task4.py
+++ hw5_task4.py
@@ -25,26 +25,46 @@ label_test = data[n-num_test:,-1]
 
 # --- Your Task --- #
 # pick five values of k by yourself 
-k_values = [1,1,1,1,1]
+k_values = [1,3,5,7,9]
 # --- end of task --- #
+
+def distance(a, b) -> float: #euclidian
+    return np.sqrt(np.sum((a - b)**2))
+
 
 er_test = []
 for k in k_values: 
     # --- Your Task --- #
     # implement the kNN classification method 
-    # ......
-    # ......
-    # ......
+    predicted_labels = []
+    # add each test point in to compare with the training set
+    for test_point in sample_test:
+        # calculate distance for each testing point, for each training point
+        distances = np.array([distance(test_point, train_point) for train_point in sample_train])
+
+        # find the nearest k values and return an array of their indexes
+        nearest_k_indexes = np.argsort(distances)[:k]
+        # use indexes to identify labels for each
+        nearest_k_labels = label_train[nearest_k_indexes]
+
+        # use the average of the labels to determine the majority winner for each class
+        avg = np.mean(nearest_k_labels)
+        predicted_label = 1 if avg >= .5 else 0
+        predicted_labels.append(predicted_label)
+
+
 
     # store classification error on testing data here 
-    er = ......
+    er = np.mean(predicted_labels != label_test)
     er_test.append(er)
 # --- end of task --- #
     
 plt.figure()    
 plt.plot(er_test)
+plt.title("Figure 6: Classification Error vs. K")
 plt.xlabel('k')
 plt.ylabel('Classification Error')
+plt.show()
 
 
 
